de293ecc251e8aca3925073f14b5cd5395042eaa
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2023-05-30 21:36:53 +0100

    Added dropdown content

--- src/web/LIVTU_MAIN/static/LIVTU_MAIN/code.js
+++ src/web/LIVTU_MAIN/static/LIVTU_MAIN/code.js
@@ -1,21 +1,21 @@
 function buttonfindclick() {
-    document.querySelectorAll("#findbutton").forEach(function(button){button.classList.toggle("open");});
-    document.querySelectorAll("#teachbutton.open").forEach(function(button){button.classList.remove("open");});
+    document.querySelectorAll(".findContent").forEach(function(button){button.classList.toggle("open");});
+    document.querySelectorAll(".teachContent.open").forEach(function(button){button.classList.remove("open");});
     document.querySelectorAll("#menu.open").forEach(function(button){button.classList.remove("open");});
     updatemenubar();
 }
 
 function buttonteachclick() {
-    document.querySelectorAll("#teachbutton").forEach(function(button){button.classList.toggle("open");});
-    document.querySelectorAll("#findbutton.open").forEach(function(button){button.classList.remove("open");});
+    document.querySelectorAll(".teachContent").forEach(function(button){button.classList.toggle("open");});
+    document.querySelectorAll(".findContent.open").forEach(function(button){button.classList.remove("open");});
     document.querySelectorAll("#menu.open").forEach(function(button){button.classList.remove("open");});
     updatemenubar();
 }
 
 function menuclick() {
     document.querySelectorAll("#menu").forEach(function(button){button.classList.toggle("open");});
-    document.querySelectorAll("#findbutton.open").forEach(function(button){button.classList.remove("open");});
-    document.querySelectorAll("#teachbutton.open").forEach(function(button){button.classList.remove("open");});
+    document.querySelectorAll(".findContent.open").forEach(function(button){button.classList.remove("open");});
+    document.querySelectorAll(".teachContent.open").forEach(function(button){button.classList.remove("open");});
     updatemenubar();
 }
 
